aae3dd435789c614db8b8f2fb2315ede3f3a2630
ElijahPavlov <capelinx@gmail.com>
2022-07-16 18:49:10 +0300

    2

--- src/View.js
+++ src/View.js
@@ -8,7 +8,7 @@ class View {
     console.clear();
     console.log(track.join(''));
     console.log('\n\n');
-    console.log(`Created by "${yourTeamName}" with love`);
+    console.log(`Created by "${Name}" with love`);
   }
 }
 
