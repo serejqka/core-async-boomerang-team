f06684b2d806eaed67169fbafb898d68ece1ff4e
serejqka <serejaseed@yandex.ru>
2022-07-16 16:07:21 +0300

    added seeders

--- db/seeders/20220716121610-demo-data.js
+++ db/seeders/20220716121610-demo-data.js
@@ -1,23 +1,18 @@
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    const users = ['Elij']
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface) {
+    const usersName = ['Elijah', 'Emma'];
+
+    const users = usersName.map((name) => ({
+      username: name,
+      score: 0,
+      createdAt: new Date(),
+      updatedAt: new Date(),
+    }))
+
+    await queryInterface.bulkInsert('Users', users);
   },
 
-  async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+  async down(queryInterface) {
+    await queryInterface.bulkDelete('Users')
   }
 };
